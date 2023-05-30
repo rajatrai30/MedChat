d23fe5b3955560b655c7f70922cde58282540aaa
Rajat Rai <mailmerairajat@mail.com>
2023-05-30 18:45:06 +0530

    Vercel Configguration

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -28,8 +28,7 @@ const Auth = () => {
 
         const { username, password, phoneNumber, avatarURL } = form;
 
-        const URL = 'http://localhost:5000/auth';
-        // const URL = 'https://medical-pager.herokuapp.com/auth';
+        const URL = 'https://med-chat-abs8qtxpz-rajatrai30.vercel.app/auth';
 
         const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
             username, password, fullName: form.fullName, phoneNumber, avatarURL,
